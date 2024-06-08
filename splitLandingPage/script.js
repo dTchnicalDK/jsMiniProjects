7dbed364fd184b0a52c63d037556ae698fd84dbc
dharmendra <dtechnicaldk@gmail.com>
2024-06-08 18:41:46 +0530

    voting click response added

--- splitLandingPage/script.js
+++ splitLandingPage/script.js
@@ -1,8 +1,12 @@
 let left = document.querySelector(".split.left");
 let right = document.querySelector(".split.right");
 let container = document.querySelector(".container");
+let VotedTo = document.querySelector(".votedTo");
+let VotedToRight = document.querySelector(".votedToRight");
+let butttonLeft = document.querySelector("button.left");
+let butttonRight = document.querySelector("button.right");
 
-// console.log(container);
+console.log(butttonLeft);
 left.addEventListener("mouseenter", () =>
   container.classList.add("hover-left")
 );
@@ -16,3 +20,12 @@ right.addEventListener("mouseenter", () =>
 right.addEventListener("mouseout", () =>
   container.classList.remove("hover-right")
 );
+
+butttonLeft.addEventListener("click", () => {
+  VotedTo.innerText = "Bjp Thanks for ur vote";
+  butttonRight.disabled = true;
+});
+butttonRight.addEventListener("click", () => {
+  VotedToRight.innerText = "congress thanks for ur vote";
+  butttonLeft.disabled = true;
+});
