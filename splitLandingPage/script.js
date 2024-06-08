c8d1bb5a88a19af826d66c1592ede6812193a679
dharmendra <dtechnicaldk@gmail.com>
2024-06-08 18:03:14 +0530

    split landing page working but no media query for small screen

--- splitLandingPage/script.js
+++ splitLandingPage/script.js
@@ -1,1 +1,18 @@
-console.log("hi from blurry loading script");
+let left = document.querySelector(".split.left");
+let right = document.querySelector(".split.right");
+let container = document.querySelector(".container");
+
+// console.log(container);
+left.addEventListener("mouseenter", () =>
+  container.classList.add("hover-left")
+);
+left.addEventListener("mouseout", () =>
+  container.classList.remove("hover-left")
+);
+
+right.addEventListener("mouseenter", () =>
+  container.classList.add("hover-right")
+);
+right.addEventListener("mouseout", () =>
+  container.classList.remove("hover-right")
+);
