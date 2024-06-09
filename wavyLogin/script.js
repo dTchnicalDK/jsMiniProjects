297f33565119d70549271b73c762a2271476eea9
dharmendra <dtechnicaldk@gmail.com>
2024-06-09 16:07:48 +0530

    Temporaty stay point to bug fix

--- wavyLogin/script.js
+++ wavyLogin/script.js
@@ -1,1 +1,17 @@
-console.log("hi from blurry loading script");
+let inputLables = document.querySelectorAll(".form-control label");
+// console.log(inputLables);
+
+inputLables.forEach(
+  (label) =>
+    (label.innerHTML = label.innerText
+      .split("")
+      .map(
+        (letter, indx) =>
+          `<span style = "transition-delay: ${indx * 80}ms">${letter}</span>`
+      )
+      .join(""))
+  //   console.log(span);
+);
+{
+  /* <span style="transition-delay: 50ms;"></span> */
+}
