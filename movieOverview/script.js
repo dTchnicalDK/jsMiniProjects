37322ff15fe16734645efbca6a64db08a38869cb
dharmendra <dtechnicaldk@gmail.com>
2024-06-25 21:20:34 +0530

    data fetched now need to update dom

--- movieOverview/script.js
+++ movieOverview/script.js
@@ -1,1 +1,18 @@
-console.log("hi from moview review script");
+let OMDB_URL = " http://www.omdbapi.com/?s=man&apikey=cc0661a6";
+// let OMDB_URL = " http://www.omdbapi.com/?plot=short&apikey=cc0661a6";
+
+async function fetchData() {
+  const response = await fetch(OMDB_URL);
+  const data = await response.json();
+  const result = data.Search;
+  result.forEach((element) => {
+    // console.log(element);
+    updateDom(element);
+  });
+}
+function updateDom(movie) {
+  const { Title, Poster } = movie;
+  const movieDiv = document.createElement("div");
+  movieDiv.classList.add("movie");
+  console.log(movieDiv);
+}
