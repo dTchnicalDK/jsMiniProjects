fd9853918a43b942a52527e6d84e4cf64d93bae2
dharmendra <dtechnicaldk@gmail.com>
2024-06-30 10:30:35 +0530

    ripple working from a constant place

--- buttonRipple/script.js
+++ buttonRipple/script.js
@@ -1,1 +1,11 @@
-console.log("hi from button ripple script");
+const buttonDiv = document.querySelector(".button-div");
+console.log(buttonDiv);
+
+buttonDiv.addEventListener("click", expand2Ripple);
+function expand2Ripple() {
+  buttonDiv.classList.add("active");
+  setTimeout(removeEffect, 900);
+}
+function removeEffect() {
+  buttonDiv.classList.remove("active");
+}
