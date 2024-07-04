5e5893c892907e523a61427e8ae5e8b4ae27cd20
dharmendra <dtechnicaldk@gmail.com>
2024-07-04 19:07:23 +0530

    initia commit ... html css and script file created

--- buttonRipple/script.js
+++ buttonRipple/script.js
@@ -4,7 +4,7 @@ console.log(buttonDiv);
 buttonDiv.addEventListener("click", expand2Ripple);
 function expand2Ripple() {
   buttonDiv.classList.add("active");
-  setTimeout(removeEffect, 900);
+  setTimeout(removeEffect, 500);
 }
 function removeEffect() {
   buttonDiv.classList.remove("active");
