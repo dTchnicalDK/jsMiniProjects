92da32260080fad3f79e45b90e9fbcb2e8839221
dharmendra <dtechnicaldk@gmail.com>
2024-07-24 21:57:16 +0530

    now nav stick on scroll working properly;

--- stickyNavbar/script.js
+++ stickyNavbar/script.js
@@ -1,1 +1,12 @@
+const navBar = document.querySelector(".nav");
+const container = document.querySelector(".nav .container");
 
+addEventListener("scroll", customiseNav);
+
+function customiseNav() {
+  if (scrollY > container.offsetHeight + 200) {
+    navBar.classList.add("active");
+  } else {
+    navBar.classList.remove("active");
+  }
+}
